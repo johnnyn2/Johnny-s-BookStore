8484b3c4c4d22c228690ac1d74304f6a7c378753
Johnny Ho Wai Kin <johnnyho@hket.com>
2020-06-24 10:51:56 +0800

    init project and install some dependencies

---
+++ client/src/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Paper} from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <Paper elevation={3}>
+        Test
+      </Paper>
+    </div>
+  );
+}
+
+export default App;
