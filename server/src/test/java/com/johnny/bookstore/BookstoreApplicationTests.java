98bc40b9cf52c1dbb14894d7f27c6603f9f7ed89
Johnny Ho Wai Kin <johnnyho@hket.com>
2020-06-24 10:02:59 +0800

    init project

---
+++ server/src/test/java/com/johnny/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnny.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
