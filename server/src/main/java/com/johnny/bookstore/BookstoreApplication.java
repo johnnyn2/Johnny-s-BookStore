5b1ce2ffe932ea8452d1e58457a2b6ae0eb07f30
Johnny Ho Wai Kin <johnnyho@hket.com>
2020-06-29 16:43:13 +0800

    backend: add date support, change main

--- server/src/main/java/com/johnny/bookstore/BookstoreApplication.java
+++ server/src/main/java/com/johnny/bookstore/BookstoreApplication.java
@@ -1,13 +1,33 @@
 package com.johnny.bookstore;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
-public class BookstoreApplication {
+@EntityScan(basePackageClasses = {
+	BookstoreApplication.class,
+	Jsr310JpaConverters.class
+})
+public class BookstoreApplication extends SpringBootServletInitializer {
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookstoreApplication.class);
+	}
 }
