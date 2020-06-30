055a5e0f90336a83e80f8ae6a9d770f7dd32fa61
Johnny Ho Wai Kin <johnnyho@hket.com>
2020-06-30 10:22:58 +0800

    backend: jwt provider, jwt authentication, security configuration

--- server/src/main/java/com/johnny/bookstore/security/CustomUserDetailsService.java
+++ server/src/main/java/com/johnny/bookstore/security/CustomUserDetailsService.java
@@ -1,5 +1,31 @@
 package com.johnny.bookstore.security;
 
-public class CustomUserDetailsService {
+import javax.transaction.Transactional;
+
+import com.johnny.bookstore.exception.ResourceNotFoundException;
+import com.johnny.bookstore.model.User;
+import com.johnny.bookstore.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail));
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException ("User", "id", id));
+        return UserPrincipal.create(user);
+    }
     
 }
--- server/src/main/java/com/johnny/bookstore/security/JwtTokenProvider.java
+++ server/src/main/java/com/johnny/bookstore/security/JwtTokenProvider.java
@@ -1,5 +1,65 @@
 package com.johnny.bookstore.security;
 
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+@Component
 public class JwtTokenProvider {
-    
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpirationInMs}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+        return Jwts.builder()
+                    .setSubject(Long.toString(userPrincipal.getId()))
+                    .setIssuedAt(now)
+                    .setExpiration(expiryDate)
+                    .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                    .compact();
+    }
+
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                            .setSigningKey(jwtSecret)
+                            .parseClaimsJws(token)
+                            .getBody();
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid Jwt Signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid Jwt Token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired Jwt Token: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported Jwt Token: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("Jwt claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
 }
