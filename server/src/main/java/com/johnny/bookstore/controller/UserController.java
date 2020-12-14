a2dc5bb5fb16fcfe8f9950055cf4c9c336c01768
Johnny Ho <johnnyhohohohohoho@gmail.com>
2020-12-14 14:51:26 +0800

    fix bug of token expire

--- server/src/main/java/com/johnny/bookstore/controller/UserController.java
+++ server/src/main/java/com/johnny/bookstore/controller/UserController.java
@@ -1,17 +1,20 @@
 package com.johnny.bookstore.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.johnny.bookstore.exception.AppException;
+import com.johnny.bookstore.model.User;
 import com.johnny.bookstore.payload.response.UserIdentityAvailability;
 import com.johnny.bookstore.payload.response.UserSummary;
 import com.johnny.bookstore.repository.UserRepository;
-import com.johnny.bookstore.security.CurrentUser;
-import com.johnny.bookstore.security.UserPrincipal;
+import com.johnny.bookstore.security.JwtTokenProvider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private JwtTokenProvider provider;
     
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -37,9 +43,22 @@ public class UserController {
     }
 
     @GetMapping("/user/me")
-    @PreAuthorize("hasRole('USER')")
-    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
-        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
-        return userSummary;
+    public ResponseEntity<?> getCurrentUser(HttpServletRequest request) {
+        String token = getJwtTokenFromRequest(request);
+        if (StringUtils.hasText(token) && provider.validateToken(token)) {
+            Long id = provider.getUserIdFromJWT(token);
+            User currentUser = userRepository.findById(id).orElseThrow(() -> new AppException("User not found with id"));
+            UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+            return ResponseEntity.ok(userSummary);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    private String getJwtTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
     }
 }
